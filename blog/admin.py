5719a044fabfd21fee8df88ebccdcf89b9d528db
Tobias Bladh <tobias.bladh@live.se>
2021-09-09 21:42:37 +0200

    Done with tutorial part 5

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# registers Post as a model that the admin view can see/manipulate
+admin.site.register(Post) 
+
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'Aug 27, 2018'
-    },
-    {
-        'author': 'Tobo the hobo',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'Aug 28, 2018'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context) #returns a HttpResponse
 
