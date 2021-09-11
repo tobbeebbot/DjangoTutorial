7c02366b2ab066eea86bfd139ade6131c4d80222
Tobias Bladh <tobias.bladh@live.se>
2021-09-11 15:17:38 +0200

    Add user registration, login, logout, profile

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
@@ -8,8 +9,12 @@ def register(request):
         if form.is_valid():
             form.save() #saves the user, hashes password etc.
             username = form.cleaned_data.get('username')   
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Your account has been created! You are now able to log in.')
             return redirect('blog-home')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
